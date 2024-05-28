9fc55743a5dd65bfe3d8f31d8abae22cb2414149
bonface221 <mainaboniface221@gmail.com>
2024-05-28 19:22:50 +0300

    fix docker-compose file and remove sensitive data

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,3 @@
-export const PAYSTACK_SECRET_KEY =
-  "sk_live_dd7944e52540d437538d1159a1315830a457a047";
-export const BASE_URL = "https://tickets.bnieaevents.com";
-export const OPENSTACK_API_KEY = "44816f8ba08949edbfebd42b170ae4ea";
+export const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
+export const BASE_URL = process.env.BASE_URL;
+export const OPENSTACK_API_KEY = process.env.OPENSTACK_API_KEY;
