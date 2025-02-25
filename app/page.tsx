4061c4474394865d1aa59b69efce9aba90e27dfa
stanleymwinde <salvadorethorbide8@gmail.com>
2025-02-25 18:39:52 +0300

    added suspense on home page dure to search params

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import { Hero } from "@/components";
 import { getUSDExchangeRate } from "@/utils";
+import { Suspense } from "react";
 
 export default async function Home() {
   const rate = await getUSDExchangeRate();
-  return <Hero rate={rate} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Hero rate={rate} />
+    </Suspense>
+  );
 }
