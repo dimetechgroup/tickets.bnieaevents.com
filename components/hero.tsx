877a25225fd51555fe671be87358367d69166af5
stanleymwinde <salvadorethorbide8@gmail.com>
2025-02-25 17:21:30 +0300

    added new input for price

--- components/hero.tsx
+++ components/hero.tsx
@@ -24,12 +24,11 @@ import {
 import { zodResolver } from "@hookform/resolvers/zod";
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import Loading from "./loader";
-import { TICKET_AMOUNT } from "@/config";
 
 const currencyOptions = [
   {
@@ -64,6 +63,22 @@ const HeroPage = ({ rate }: { rate: number }) => {
   });
   const selectedCurrency = watch("currency");
   const noOfTickets = watch("numberOfTickets");
+
+  const searchParams = useSearchParams();
+
+  const price = searchParams.get("price");
+
+  const ticketprice = () => {
+    if (price === "JDUw") {
+      return 50;
+    } else {
+      return 120;
+    }
+  };
+
+  const TICKET_AMOUNT = ticketprice();
+
+  console.log(TICKET_AMOUNT);
 
   const onSubmit = async (data: FormData) => {
     setIsLoading(true);
@@ -140,7 +155,7 @@ const HeroPage = ({ rate }: { rate: number }) => {
         >
           <Heading fontSize="xl" color="brand.main" fontStyle="italic">
             <Box as="span" color="brand.black">
-              Early Bird {"->"}
+              Ticket Price {"->"}
             </Box>{" "}
             {getHeading()}
           </Heading>
--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,5 @@
+import TicketPrice from "./ticketPrice";
+
 export const PAYSTACK_SECRET_KEY =
   process.env.PAYSTACK_SECRET_KEY ||
   "sk_live_dd7944e52540d437538d1159a1315830a457a047";
@@ -5,5 +7,3 @@ export const BASE_URL =
   process.env.BASE_URL || "https://tickets.bnieaevents.com";
 export const OPENSTACK_API_KEY =
   process.env.OPENSTACK_API_KEY || "44816f8ba08949edbfebd42b170ae4ea";
-
-export const TICKET_AMOUNT = 199;
